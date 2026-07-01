2fdf85be451c3a661e7c15af69af28006b501adf
Joy Yu Wen <wenjoyyu@gmail.com>
2026-07-01 10:29:21 +0800

    combined my code with runner, but main code updated

--- runner.cpp
+++ runner.cpp
@@ -138,6 +138,130 @@ public:
     }
 };
 
+// ====== Member 3 ======
+
+class Instruction {
+    public:
+        virtual ~Instruction() {}
+
+        virtual void execute(CPU& cpu) = 0;
+        virtual string getName() const =0;
+};
+
+//handle specific LIFO (Last-In, First-Out) stack or register-based fetching protocols.
+class ArithmeticIns : public Instruction {
+protected:
+    virtual signed char compute(signed char a, signed char b) = 0;
+
+public:
+    //Stack architecture, Right-to-Left (First out is operand B)
+    void execute(CPU& cpu) override {
+        signed char b = cpu.stackPop(); 
+        signed char a = cpu.stackPop();
+
+        signed char result = compute(a, b);
+
+        // Push the calculated value back to the stack
+        cpu.stackPush(result);
+    }
+};
+
+// my own
+class ADDInstruction : public ArithmeticIns {
+    protected:
+        signed char compute(signed char a, signed char b) override {
+            return a + b;
+        }
+
+    public:
+        string getName() const override {
+            return "ADD";
+        }
+};
+
+class SUBInstruction : public ArithmeticIns {
+    protected:
+        signed char compute(signed char a, signed char b) override {
+            return a - b;
+        }
+    
+    public: 
+        string getName() const override {
+            return "SUB";
+        }
+};
+
+class MULInstruction : public ArithmeticIns {
+    protected:
+        signed char compute(signed char a, signed char b) override {
+            return a * b;
+        }
+
+    public:
+        string getName() const override {
+            return "MUL";
+        }
+};
+
+class DIVInstruction : public ArithmeticIns {
+    protected:
+        signed char compute(signed char a, signed char b) override {
+            //error handling
+            if (b == 0){
+                cerr << "[ERROR] DIV by zero\n";
+                return 0;
+            }
+            //other outputs
+                //print error
+                //halt VM
+                //throw exception
+
+            return a / b;
+        }
+
+    public:
+        string getName() const override {
+            return "DIV";
+        }
+};
+
+//Increment
+class INCInstruction : public Instruction { 
+    public:
+        void execute(CPU& cpu) override {
+            signed char value = cpu.stackPop();
+            cpu.stackPush(value + 1);
+        }
+
+        string getName() const override {
+            return "INC";
+        }
+}; 
+
+//Decrement
+class DECInstruction : public Instruction {
+    public:
+        void execute(CPU& cpu) override {
+            signed char value = cpu.stackPop();
+            cpu.stackPush(value - 1);
+        }
+
+    string getName() const override {
+        return "DEC";
+    }
+};  
+
+// //state/pointer reset 
+// class RESETInstruction : public Instruction {
+//     public:
+//         void execute(CPU& cpu) override {
+//            while (!cpu.stackIsEmpty()) {
+//                cpu.stackPop(); or cpu.resetPC()
+//         }
+//     }
+// }; 
+
+
 static string trim(const string& text)
 {
     size_t start = 0;
